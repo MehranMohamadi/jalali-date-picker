5ef70844cf48f28328b4524b6c0abe061a79bfb3
Mehran <mohammadi000098@gmail.com>
2026-09-25 18:27:20 +0330

    feat: update cloud sync configuration and environment setup in README and example files

--- api/cloud-sync.ts
+++ api/cloud-sync.ts
@@ -1,5 +1,5 @@
 import type { IncomingMessage, ServerResponse } from 'node:http'
-import { getCloudConfig, hasCloudSession, proxyCloudSnapshot, sameOrigin } from '../src/server/cloudAccess'
+import { getCloudConfig, hasCloudSession, proxyCloudSnapshot, sameOrigin } from '../src/server/cloudAccess.js'
 
 export default async function handler(request: IncomingMessage & { body?: unknown }, response: ServerResponse) {
   response.setHeader('Cache-Control', 'no-store')
